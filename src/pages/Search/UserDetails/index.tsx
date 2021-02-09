69a5c6a3aab08d6c6bc0b1219fde0cb2e2629c20
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-02-09 19:40:29 -0300

    Finished project

--- src/pages/Search/UserDetails/index.tsx
+++ src/pages/Search/UserDetails/index.tsx
@@ -13,14 +13,44 @@ const UserDetails = ({user}: Props) => {
     }
 
     return (
-        <div>
+        <>
             <div className={`search-content-user ${user.login !== '' ? 'active' : ''}`}>
+
                 <img src={user.avatar_url} alt="" className="search-content-image"/>
+                <div className="search-content-actions">
+                    <Button text="Ver Perfil" handleOnClick={handleOnClick}/>
+                </div>
             </div>
-            <div className="search-content-actions">
-                <Button text="Ver Perfil" handleOnClick={handleOnClick}/>
+            <div className="search-content-infos">
+                <div className="search-content-infos-bar">
+                    <div className="info repo-public br-5 ">
+                        Repositorios públicos: {user.public_repos}
+                    </div>
+                    <div className="info followers br-5 ">
+                        Seguidores: {user.followers}
+                    </div>
+                    <div className="info following br-5 ">
+                        Seguindo: {user.following}
+                    </div>
+                </div>
+                <div className="search-content-infos-fields br-5">
+                    <h3 className="title-infos">Informações</h3>
+                    <div className="info principal br-4">
+                        <strong>Empresa: </strong> {user.company} 
+                    </div>
+                    <div className="info principal br-4">
+                        <strong>Website/Blog: </strong> {user.blog} 
+                    </div>
+                    <div className="info principal br-4">
+                        <strong>Localidade: </strong> {user.location} 
+                    </div>
+                    <div className="info principal br-4">
+                        <strong>Membro desde: </strong> {user.created_at} 
+                    </div>
+                </div>
             </div>
-        </div>
+            
+        </>
     );
 }
 
